ab3814692bb0a8a87f79556b7591efb7024c3e75
⛧-440729 [sophie] <sophie@999eagle.moe>
2024-07-18 11:16:04 +0200

    python3Packages.python-hglib: remove nose dependency, fix tests on python 3.12

--- pkgs/development/python-modules/python-hglib/default.nix
+++ pkgs/development/python-modules/python-hglib/default.nix
@@ -1,9 +1,10 @@
 {
   lib,
+  pythonAtLeast,
   buildPythonPackage,
+  fetchpatch,
   fetchPypi,
   mercurial,
-  nose,
 }:
 
 buildPythonPackage rec {
@@ -15,11 +16,21 @@ buildPythonPackage rec {
     inherit pname version;
     hash = "sha256-sYvR7VPJDuV9VxTWata7crZOkw1K7KmDCJLAi7KNpgg=";
   };
+  patches =
+    [
+      # this patch removes the nose dependency for testing but isn't yet part of a released version
+      (fetchpatch {
+        url = "https://repo.mercurial-scm.org/python-hglib/raw-rev/8341f2494b3f";
+        excludes = [ "heptapod-ci.yml" ];
+        hash = "sha256-WFljjLJ/9BSlkp2MvhZtb0gLYTWFmwYJnpgbiUpBSbU=";
+      })
+    ]
+    ++ lib.optionals (pythonAtLeast "3.12") [
+      # most tests currently fail on python3.12 due to `assertEquals` being removed
+      ./python312-test-fix.patch
+    ];
 
-  nativeCheckInputs = [
-    mercurial
-    nose
-  ];
+  nativeCheckInputs = [ mercurial ];
 
   preCheck = ''
     export HGTMP=$(mktemp -d)
